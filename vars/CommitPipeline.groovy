e78c7a51fd8cb4a29584092a92843509e9d7f9b3
Kuleshov Dmitry <dkuleshov@lohika.com>
2019-04-05 14:00:27 +0300

    rewrite CommitPipeline

--- vars/CommitPipeline.groovy
+++ vars/CommitPipeline.groovy
@@ -2,37 +2,24 @@ def call(body) {
     // Init the MPL library
     MPLInit()
 
-    // Executing the pipeline without additional configuration
-    pipeline {  // Declarative pipeline
-        agent {
-            label 'master'
-        }
-        stages {
-            stage('Checkout') {
-                steps {
-                    MPLModule()
-                }
-            }
-            stage('Analyze') {
-                steps {
-                    MPLModule('Analyze', [
-                            sonarqube: [
-                                    server: 'sonar_server'
-                            ]
-                    ])
-                }
-            }
-            stage('Build') {
-                steps {
-                    MPLModule('Build', [ // Using overriden Maven Build
-                                         maven: [
-                                                 tool           : 'M3',
-                                                 settings_config: 'nexus_maven_settings',
-                                                 extra_args     : '-DskipTests'
-                                         ]
-                    ])
-                }
-            }
-        }
+    MPLPipeline {
+        agent_label: 'master'
+        modules:
+        [
+                Checkout: [:],
+                Analyze : [:],
+                Build   : [:]
+        ]
+        maven:
+        [
+                tool           : 'M3',
+                settings_config: 'nexus_maven_settings',
+                extra_args     : '-DskipTests'
+        ]
+        sonarqube:
+        [
+                server: 'sonar_server'
+        ]
     }
+
 }
